60fa978f35feeb475d26155a174827ffe4bbe632
Joseph Peck <joe.r.peck@gmail.com>
2023-11-28 01:28:53 -0800

    MTG mode selection

---
+++ src/services/request-services/prismatic-request-service.ts
@@ -0,0 +1,11 @@
+import { AbstractRequestService } from "./abstract-request-service";
+
+export class PrismaticRequestService extends AbstractRequestService {
+  constructor()  {
+    super();
+  }
+
+  async getDeckStandings(league: number): Promise<any> {
+    return this.get(`/prismatic/standings/${league}`);
+  }
+}
